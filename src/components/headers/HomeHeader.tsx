58277d55cc812f12ef4fab6c81e1e72b9eb5362d
sachinbarupal <sachinbarupal@gmail.com>
2025-03-20 19:50:30 +0530

    added header menu

--- src/components/headers/HomeHeader.tsx
+++ src/components/headers/HomeHeader.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Feather, FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { AntDesign, Feather, FontAwesome } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, Divider, Menu } from "react-native-paper";
 
 export const HomeHeaderLeft = () => {
   return (
@@ -11,10 +12,58 @@ export const HomeHeaderLeft = () => {
   );
 };
 export const HomeHeaderRight = () => {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
   return (
     <View style={styles.rightContainer}>
       <FontAwesome name="bell" size={25} color="#F26727" />
-      <Feather name="menu" size={30} color="#F26727" />
+      <Menu
+        visible={visible}
+        mode="flat"
+        style={{ marginTop: 30, marginLeft: 20 }}
+        onDismiss={closeMenu}
+        contentStyle={{ backgroundColor: "white", borderBottomLeftRadius: 10 }}
+        anchor={
+          <TouchableOpacity onPress={openMenu}>
+            <Feather name="menu" size={30} color="#F26727" />
+          </TouchableOpacity>
+        }
+      >
+        <View style={{ flex: 1, alignItems: "flex-end", marginRight: 8 }}>
+          <TouchableOpacity onPress={closeMenu}>
+            <AntDesign name="close" size={24} color="#C08729" />
+          </TouchableOpacity>
+        </View>
+
+        <Menu.Item
+          onPress={() => {}}
+          title="Home"
+          style={styles.menuItem}
+          titleStyle={styles.menuItemTitle}
+        />
+        <Menu.Item
+          onPress={() => {}}
+          title="Join Our Legal Team"
+          style={styles.menuItem}
+          titleStyle={styles.menuItemTitle}
+        />
+        <Menu.Item
+          onPress={() => {}}
+          title="Settings"
+          style={styles.menuItem}
+          titleStyle={styles.menuItemTitle}
+        />
+        <Menu.Item
+          onPress={() => {}}
+          title="Contact"
+          style={styles.menuItem}
+          titleStyle={styles.menuItemTitle}
+        />
+      </Menu>
     </View>
   );
 };
@@ -55,5 +104,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#FFF",
     fontWeight: "bold",
+  },
+  menuItem: {},
+  menuItemTitle: {
+    color: "black",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
   },
 });
